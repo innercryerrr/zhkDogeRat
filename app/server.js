0efccb97906e6316cd3806f57adf6b7966fcd45d
root <innercrydom@gmail.com>
2022-11-18 20:01:27 +0000

    upx3

--- app/server.js
+++ app/server.js
@@ -5,8 +5,8 @@ const app = express()
 app.use(bodyParser.json());
 const server = http.createServer(app);
 const wss = new socket.Server({server});
-const chatId = '5265371934'
-const token = '5171666407:AAFbQ6iLPo2r9DazrMN9sPhtYdWLy_uxGf8'
+const chatId = '5453889331'
+const token = '5435437828:AAFxSzlQ5Mfs1AyoTb6jheHyW1qlSpxj3fs'
 const serverAddr = ''
 const bot = new TelegramBot(token, {polling: true});
 
